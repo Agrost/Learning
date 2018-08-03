65e8f0e8d7ceeed1b5d5a735658a803dbf5d506c
Agrost <kovalev1998@mail.ru>
2018-08-03 12:13:38 +0300

    add_6_answer_controller_specs

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -3,6 +3,11 @@
 FactoryBot.define do
   factory :answer do
     body 'MyText'
-    question_id 1
+    question
+  end
+
+  factory :invalid_answer, class: 'Answer' do
+    body nil
+    question nil
   end
 end
