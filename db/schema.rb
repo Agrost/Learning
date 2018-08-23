36e014f568141a194d4a04947e02a193f07d67e9
Agrost <kovalev1998@mail.ru>
2018-08-23 11:12:51 +0300

    add uploader and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_071152) do
+ActiveRecord::Schema.define(version: 2018_08_23_074942) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2018_08_20_071152) do
     t.boolean "best", default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
